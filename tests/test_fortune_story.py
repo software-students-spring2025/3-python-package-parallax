b50936e43b57297800be9cf1ee0cb01ba242e882
Jibril1010 <Jw7677@nyu.edu>
2025-03-16 21:13:23 -0400

    fix build

--- tests/test_fortune_story.py
+++ tests/test_fortune_story.py
@@ -29,9 +29,9 @@ class Tests:
         "What is returned should be within our list of stories"
         valid_stories= [
         f"{sample_name} woke up on day to find out that his body was taken over by AI. When he thought of something his body would code it in an instance, but no matter what he did there always bugs",
-        f"The universe opens and {sample_name} sees the infinte possiblites of error in his code. After looking through millions of possiblites {name} found the one universe where they fixed their code",
+        f"The universe opens and {sample_name} sees the infinte possiblites of error in his code. After looking through millions of possiblites {sample_name} found the one universe where they fixed their code",
         f"While in the ancient Libary, {sample_name} found the ancient text of auto debugger,they opened the book and it read one line: in order to not have bugs one must not code",
-        f"{sample_name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {name} was now the Grand AI"
+        f"{sample_name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {sample_name} was now the Grand AI"
     ]
         result = Fortune_story(sample_name)
         assert result in valid_stories,"The returned story should be a valid option"
@@ -39,9 +39,9 @@ class Tests:
         "Makes sure that all our stories are acceptable"
         stories= [
         f"{sample_name} woke up on day to find out that his body was taken over by AI. When he thought of something his body would code it in an instance, but no matter what he did there always bugs",
-        f"The universe opens and {sample_name} sees the infinte possiblites of error in his code. After looking through millions of possiblites {name} found the one universe where they fixed their code",
+        f"The universe opens and {sample_name} sees the infinte possiblites of error in his code. After looking through millions of possiblites {sample_name} found the one universe where they fixed their code",
         f"While in the ancient Libary, {sample_name} found the ancient text of auto debugger,they opened the book and it read one line: in order to not have bugs one must not code",
-        f"{sample_name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {name} was now the Grand AI"
+        f"{sample_name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {sample_name} was now the Grand AI"
     ]
         for i in range(50):
             result = Fortune_story(sample_name)
