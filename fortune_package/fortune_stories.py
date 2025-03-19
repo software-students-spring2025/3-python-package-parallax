3c0f20141b9d1e7657bf8a6995dbfcc45ebad6ef
Jibril1010 <Jw7677@nyu.edu>
2025-03-19 09:14:42 -0400

    added stories

--- fortune_package/fortune_stories.py
+++ fortune_package/fortune_stories.py
@@ -4,6 +4,12 @@ def fortune_story(name):
         f"{name} woke up on day to find out that his body was taken over by AI. When he thought of something his body would code it in an instance, but no matter what he did there always bugs",
         f"The universe opens and {name} sees the infinte possiblites of error in his code. After looking through millions of possiblites {name} found the one universe where they fixed their code",
         f"While in the ancient Libary, {name} found the ancient text of auto debugger,they opened the book and it read one line: in order to not have bugs one must not code",
-        f"{name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {name} was now the Grand AI"
+        f"{name} searched far and wide in order to find the Grand AI that would save the world. After finding it the world turned into 1s and 0s and {name} was now the Grand AI",
+        f"{name}  travled through the broken world filled with AI and robots they were the last savior of the World they were the last software engineer",
+        f"{name} spent many sleepless nights looking for bugs in their code until they noticed they were the bug in the code",
+        f"{name} was on a plane flying and it broke apart mid flight, but with their coding skills they hackedd into the world and teleprted to their destinnation",
+        f"{name} was on their last leg breathing heavy, but they couldnt give up everyone was counting on {name} and they mereged the pull request",
+        f"{name} was fighting against the robot empire and beat them by saying the forbidden digit '1' ",
+        f"{name} broke their computer and jumped in to debug their code and was never seen again" 
     ]
     return random.choice(stories)
