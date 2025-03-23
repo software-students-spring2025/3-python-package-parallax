55074331f7efcb2e3380d3be6d87e764e78065b0
Jibril1010 <Jw7677@nyu.edu>
2025-03-23 00:49:26 -0400

    py fortune update

--- tests/test_fortune_story.py
+++ tests/test_fortune_story.py
@@ -44,7 +44,7 @@ class Tests:
     def test_all_stories(self,sample_name):
         "Makes sure that all our stories are acceptable"
         stories= [
-          f"{sample_name} will wake up one day to find out that his body was taken over by AI. When {sample_name} thought of something, {sample_name}'s body would code it in an instance, but no matter what {sample_name} did, there were always bugs...",
+           f"{sample_name} will wake up one day to find out that their body was taken over by AI. When {sample_name} thought of something, {sample_name}'s body would code it in an instance, but no matter what {sample_name} did, there were always bugs...",
         f"The universe will open and {sample_name} will see the infinte possiblites of error in their code. After looking through millions of possiblites, {sample_name} finally will find the one universe where they will fix their code! Yay!",
         f"While in the ancient Libary, {sample_name} found the ancient text of auto debugger,they opened the book and it read one line: in order to not have bugs one must not code",
         f"{sample_name} will search far and wide in order to find the Grand AI that would save the world. After finding it, the world will turn into binary 1s and 0s, and {sample_name} will become the Grand AI...spooky.",
